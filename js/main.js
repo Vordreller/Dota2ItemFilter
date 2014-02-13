03117929776a107d8b1d6298a3f0102e95579d60
KoenDG <kdg.private@gmail.com>
2014-02-13 12:47:18 +0100

    Some changes for Chrome specific behavior.

--- js/main.js
+++ js/main.js
@@ -192,8 +192,9 @@ document.addEventListener('DOMContentLoaded', function() {
 
         //Send the proper header information along with the request
         http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        http.setRequestHeader("Content-length", params.length);
-        http.setRequestHeader("Connection", "close");
+		//Chrome doesn't allow these.
+        //http.setRequestHeader("Content-length", params.length);
+        //http.setRequestHeader("Connection", "close");
 
         http.onreadystatechange = function() {//Call a function when the state changes.
             if (http.readyState == 4 && http.status == 200) {
